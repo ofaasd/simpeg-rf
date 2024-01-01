7963a163a06bedb3d44afb73c656f884718e20b2
ofaasd <ofaasd@gmail.com>
2024-01-02 03:52:15 +0700

    adding csrf on validasi form

--- resources/views/admin/psb/validasi.blade.php
+++ resources/views/admin/psb/validasi.blade.php
@@ -40,6 +40,7 @@
     </div>
     <div class="offcanvas-body mx-0 flex-grow-0">
       <form class="add-new-{{strtolower($title)}} pt-0" id="addNew{{$title}}Form">
+        @csrf
         <input type="hidden" name="id" id="{{strtolower($title)}}_id">
         <div class="form-floating form-floating-outline mb-4">
           <input type="text" class="form-control" id="add-{{strtolower($title)}}-bank" placeholder="bank Pegirim | Ex : BRI" name="bank" />
