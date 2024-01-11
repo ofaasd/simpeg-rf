9e65aa282dbb5966fecf45803cc8f5aa9b1bc200
ofaasd <ofaasd@gmail.com>
2024-01-11 12:22:32 +0700

    update add tosql

--- app/Http/Controllers/admin/psb.php
+++ app/Http/Controllers/admin/psb.php
@@ -862,6 +862,7 @@ https://psb.ppatq-rf.id';
     $user = UserPsb::all();
     foreach ($user as $row) {
       $psb_peserta = PsbPesertaOnline::where('no_pendaftaran', 'RF.ppatq.011.23');
+      echo $psb_peserta->toSql() . 'RF.ppatq.011.23';
       if ($psb_peserta->count() > 0) {
         $psb_peserta = $psb_peserta->first();
         $update_user = UserPsb::find($row->id);
