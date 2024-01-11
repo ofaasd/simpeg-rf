20e37c4c4286ae4eaefe05f2498f518adb617f32
ofaasd <ofaasd@gmail.com>
2024-01-11 11:09:50 +0700

    update query

--- app/Http/Controllers/admin/HomePage.php
+++ app/Http/Controllers/admin/HomePage.php
@@ -60,7 +60,7 @@ class HomePage extends Controller
     $jumlah_santri_lapor = Pembayaran::whereMonth('tanggal_bayar', $bulan)
       ->whereYear('tanggal_bayar', $tahun)
       ->groupBy('nama_santri');
-    $jumlah_siswa_belum_lapor = $jumlah_santri_lapor->toSql();
+    $jumlah_siswa_belum_lapor = $jumlah_santri_lapor->toSql() . ' ' . $bulan . ' ' . $tahun;
 
     if ($bulan == 1) {
       $bulan = 13;
