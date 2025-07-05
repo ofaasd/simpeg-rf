a3ae830108071740b1ea526c0b54bcea041ac064
Abdul Ghofar <ofaasd@gmail.com>
2025-07-05 13:28:41 +0700

    update select santri detail nama

--- app/Http/Controllers/admin/AdminDetailTahfidzController.php
+++ app/Http/Controllers/admin/AdminDetailTahfidzController.php
@@ -84,7 +84,7 @@ class AdminDetailTahfidzController extends Controller
         //   ->limit($limit)
         //   ->orderBy($order, $dir)
         //   ->get();
-        $detail = DetailSantriTahfidz::where('id_tahfidz', $tahfidz->id)
+        $detail = DetailSantriTahfidz::select('santri_detail.nama','detail_santri_tahfidz.*','kode_juz.nama as kode_juz_surah')->where('id_tahfidz', $tahfidz->id)
           ->join('santri_detail', 'detail_santri_tahfidz.no_induk', '=', 'santri_detail.no_induk')
           ->offset($start)
           ->limit($limit)
@@ -105,7 +105,8 @@ class AdminDetailTahfidzController extends Controller
         //   ->limit($limit)
         //   ->orderBy($order, $dir)
         //   ->get();
-        $detail = DetailSantriTahfidz::where('id_tahfidz', $tahfidz->id)
+        $detail = DetailSantriTahfidz::select('santri_detail.nama','detail_santri_tahfidz.*','kode_juz.nama as kode_juz_surah')
+          ->where('id_tahfidz', $tahfidz->id)
           ->where(function ($query) use ($search) {
             $query
               ->where('id', 'LIKE', "%{$search}%")
